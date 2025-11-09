37b07d87fb4bd84ac41f27fc7e156745ac7efd04
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-11-09 15:28:21 +0000

    Added from import library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view: List all books
 def list_books(request):

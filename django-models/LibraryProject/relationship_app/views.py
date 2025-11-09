8b3a7db34c6499989602d6b68f84a6adff69eb1e
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-11-09 15:31:16 +0000

    Added from django.views.generic...

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 # relationship_app/views.py
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 

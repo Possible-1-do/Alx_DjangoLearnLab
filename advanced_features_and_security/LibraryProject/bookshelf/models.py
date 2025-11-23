dae981f5e7f1f589d04eadca189a1f8634c0fe07
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-11-23 22:14:49 +0000

    Default date set

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    published_date = models.DateField()
 
-    def __str__(self):
-        return f"{self.title} by {self.author}"
+def __str__(self):
+    return self.title
 
 
 # Create your models here.

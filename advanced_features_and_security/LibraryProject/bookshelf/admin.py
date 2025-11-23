d009b49123094639fa4afaf076f460f2a6987ee9
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-11-23 23:07:16 +0000

    admin.py added

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book, UserProfile
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
+admin.site.register(UserProfile)
+
 
 # Register your models here.

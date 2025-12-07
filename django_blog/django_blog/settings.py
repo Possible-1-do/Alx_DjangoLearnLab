72857ed7c1f1283ab42b9d05dbba849d89e1cedc
Anyomi Godwin <www.anyomigodwin2871@gmail.com>
2025-12-07 08:43:17 +0000

    Removal of postgres to reinstate sqlite

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
